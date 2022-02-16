e57b36de89b53c1bf24b9ed2091570fbc7ed20d8
NETSCH Christoph <christoph.netsch@csem.ch>
2022-02-16 19:12:39 +0100

    undid error

--- tests/test_flaskstarter.py
+++ tests/test_flaskstarter.py
@@ -2,9 +2,9 @@
 
 import pytest
 
-from ..cve_manager_app import create_app
-from ..cve_manager_app.extensions import db
-from ..cve_manager_app.user import Users
+from ..ame_manager_app import create_app
+from ..ame_manager_app.extensions import db
+from ..ame_manager_app.user import Users
 
 
 def test_home_page():
